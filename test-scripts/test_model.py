a2633090ba66befc0949dc845f8a2499ee234f00
hsorby <h.sorby@auckland.ac.nz>
2021-08-25 20:52:05 +1200

    Simplify test_model.py script.

--- test-scripts/test_model.py
+++ test-scripts/test_model.py
@@ -16,41 +16,32 @@ class TranslationTestCase(unittest.TestCase):
         self._p = lc.Parser()
         self._v = lc.Validator()
 
-    def test_basic_model(self):
-        translated_model = run_saxon('basic_model.cellml')
+    def _validate_model(self, model_name):
+        translated_model = run_saxon(model_name)
 
         m = self._p.parseModel(translated_model)
 
         self._v.validateModel(m)
+
+    def test_basic_model(self):
+        self._validate_model('basic_model.cellml')
 
         self.assertEqual(0, self._v.errorCount())
 
     def test_van_der_pol_model(self):
-        translated_model = run_saxon('van_der_pol_model.cellml')
-
-        m = self._p.parseModel(translated_model)
-
-        self._v.validateModel(m)
+        self._validate_model('van_der_pol_model.cellml')
 
         self.assertEqual(0, self._v.errorCount())
 
     def test_cellml_1_0_model(self):
-        translated_model = run_saxon('cellml1.0.xml')
-
-        m = self._p.parseModel(translated_model)
-
-        self._v.validateModel(m)
+        self._validate_model('cellml1.0.xml')
 
         self.assertEqual(2, self._v.errorCount())
         self.assertIn('celsius', self._v.error(0).description())
         self.assertEqual('1.3.1.2', self._v.error(1).referenceHeading())
 
     def test_cellml_1_1_model(self):
-        translated_model = run_saxon('cellml1.1.xml')
-
-        m = self._p.parseModel(translated_model)
-
-        self._v.validateModel(m)
+        self._validate_model('cellml1.1.xml')
 
         self.assertEqual(2, self._v.errorCount())
         self.assertIn('celsius', self._v.error(0).description())
